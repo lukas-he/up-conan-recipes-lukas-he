e38c0af6f856e50e185a969f18d7edfa6fd397ea
Lukas Heppel <lukas.heppel@d-fine.com>
2025-03-14 22:58:15 +0100

    add version to conanfile

--- up-transport-zenoh-cpp/developer/conanfile.py
+++ up-transport-zenoh-cpp/developer/conanfile.py
@@ -6,6 +6,7 @@ from conan.tools.files import copy
 
 class upZenohTransportRecipe(ConanFile):
     name = "up-transport-zenoh-cpp"
+    version = "main"
 
     # Optional metadata
     license = "Apache-2.0"
